78c7417127799e15e8d6f2a31dde00a848516963
moniversity23 <chidinmaifekanfu@gmail.com>
2025-10-09 12:38:14 +0100

    🔧 Fixed offline page mismatch (.ejs)

--- public/service-worker.js
+++ public/service-worker.js
@@ -42,7 +42,7 @@ self.addEventListener("fetch", (event) => {
       })
       .catch(() =>
         caches.match(event.request).then(
-          (res) => res || caches.match("/offline.html")
+          (res) => res || caches.match("/offline.ejs")
         )
       )
   );
